66541f7d675ca551b13afa53babe3cb7ad69ec43
Guilherme <guilhermegm.andrade@gmail.com>
2022-05-15 21:16:11 -0300

    exercio commonJS

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const Planet = require('./planet')
+const infoConvert = require('./infoConvert')
+
+let planets = [
+    new Planet("Mercurio", 0.39),
+    new Planet("Venus", 0.72),
+    new Planet("Terra", 1)
+]
+
+planets.forEach(planetas => {
+    let distanceInKm = infoConvert.convertAUtoKm(planetas.auToSun).toFixed(2)
+    console.log(`${planetas.name} - ${planetas.auToSun}AU - ${distanceInKm}km`)
+})
